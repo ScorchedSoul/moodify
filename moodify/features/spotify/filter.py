bf0070753a948cfbe50a60f94625be00f0690e83
AceHunterr <aggarwalmehul26@gmail.com>
2023-01-17 03:55:52 +0530

    Optimized Sorting logic

--- moodify/features/spotify/filter.py
+++ moodify/features/spotify/filter.py
@@ -30,7 +30,7 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scopes))
 
 
 def retrieve_songs():
-    top_tracks = sp.current_user_top_tracks(limit=10)
+    top_tracks = sp.current_user_top_tracks(limit=50)
 
     for idx, item in enumerate(top_tracks['items']):
         uri = item['uri']
@@ -38,58 +38,67 @@ def retrieve_songs():
         energy = sp.audio_features(uri)[0]['energy']
         danceability = sp.audio_features(uri)[0]['danceability']
         valence = sp.audio_features(uri)[0]['valence']
-        print(idx, item['uri'], item['name'], [a['name'] for a in item['artists']],valence)
+        print(idx, item['uri'], item['name'], [a['name'] for a in item['artists']])
         
         if valence > 0.5:
             happy_songs.append(item)
+            item.update({"valence" : valence})
            
         else:
             sad_songs.append(item)
+            item.update({"valence" : valence})
         
         if energy>0.6:
             high_energy_songs.append(item)
+            item.update({"energy" : energy})
         else:
             calm_songs.append(item)
+            item.update({"energy" : energy})
+
         
         if danceability>0.7:
             cheerful_songs.append(item)
+            item.update({"danceability" : danceability})
+
+        
 
 
 
 retrieve_songs()
 
 print("LIST OF HAPPY SONGS")
-happy_songs = sorted(happy_songs, key=lambda k: [sp.audio_features(k['uri'])[0]['valence']])
+happy_songs = sorted(happy_songs, key=lambda k: k["valence"])
 happy_songs.reverse()
 for item in happy_songs:
-    print(f"Song: {item['name']}, Artists: {[a['name'] for a in item['artists']]} , {sp.audio_features(item['uri'])[0]['valence']}")
+    # print(item)
+    print(f"Song: {item['name']}, Artists: {[a['name'] for a in item['artists']]} , {item['valence']} ")
 
 
 
 print("LIST OF SAD SONGS")
-sad_songs = sorted(sad_songs, key=lambda k: [sp.audio_features(k['uri'])[0]['valence']])
+sad_songs = sorted(sad_songs, key=lambda k: k["valence"])
 for item in sad_songs:
-    print(f"Song: {item['name']}, Artists: {[a['name'] for a in item['artists']]} , {sp.audio_features(item['uri'])[0]['valence']}")
+    print(f"Song: {item['name']}, Artists: {[a['name'] for a in item['artists']]} , {item['valence']} ")
 
 
 print("LIST OF HIGH ENERGY SONGS")
-high_energy_songs = sorted(high_energy_songs, key=lambda k: [sp.audio_features(k['uri'])[0]['energy']])
+high_energy_songs = sorted(high_energy_songs, key=lambda k: k["energy"])
 high_energy_songs.reverse()
 for item in high_energy_songs:
-    print(f"Song: {item['name']}, Artists: {[a['name'] for a in item['artists']]},{sp.audio_features(item['uri'])[0]['energy']}" )
+    print(f"Song: {item['name']}, Artists: {[a['name'] for a in item['artists']]} , {item['energy']} " )
 
 
 print("LIST OF CALM SONGS")
-calm_songs = sorted(calm_songs, key=lambda k: [sp.audio_features(k['uri'])[0]['energy']])
+calm_songs = sorted(calm_songs, key=lambda k: k["energy"])
 for item in calm_songs:
-    print(f"Song: {item['name']}, Artists: {[a['name'] for a in item['artists']]},{sp.audio_features(item['uri'])[0]['energy']}")
+    print(f"Song: {item['name']}, Artists: {[a['name'] for a in item['artists']]} , {item['energy']} ")
 
 
 print("LIST OF CHEERFUL SONGS")
-cheerful_songs = sorted(cheerful_songs, key=lambda k: [sp.audio_features(k['uri'])[0]['danceability']])
+cheerful_songs = sorted(cheerful_songs, key=lambda k: k["danceability"])
 cheerful_songs.reverse()
 for item in cheerful_songs:
-    print(f"Song: {item['name']}, Artists: {[a['name'] for a in item['artists']]},{sp.audio_features(item['uri'])[0]['danceability']}")
+    print(f"Song: {item['name']}, Artists: {[a['name'] for a in item['artists']]} , {item['danceability']} ")
 
 
 
